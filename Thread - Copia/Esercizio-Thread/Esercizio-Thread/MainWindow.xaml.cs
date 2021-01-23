a69f662e8c0657c480210287f5b8f8bf0ab9737b
leonardo969 <leonardo.vincenzi.stud@ispascalcomandini.it>
2021-01-23 09:18:12 +0100

    /

---
+++ Thread - Copia/Esercizio-Thread/Esercizio-Thread/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Esercizio_Thread
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly Uri uriMacchina = new Uri("Maccchina.png", UriKind.Relative);
+        int posMacchina;
+
+        readonly Uri uriAereo = new Uri("Aereo.png", UriKind.Relative);
+        int posAereo;
+
+        readonly Uri uriMessi = new Uri("Messi.png", UriKind.Relative);
+        int posMessi;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Thread t1 = new Thread(new ThreadStart(muoviMacchina));
+
+            ImageSource im = new BitmapImage(uriMacchina);
+            imgMacchina.Source = im;
+
+            t1.Start();
+
+            Thread t2 = new Thread(new ThreadStart(muoviAereo));
+
+            ImageSource im2 = new BitmapImage(uriAereo);
+            imgAereo.Source = im2;
+
+            t2.Start();
+
+            Thread t3 = new Thread(new ThreadStart(muoviMessi));
+
+            ImageSource im3 = new BitmapImage(uriMessi);
+            imgMessi.Source = im3;
+
+            t3.Start();
+
+            lbl.Content = "I concorrenti sono arrivati a parimerito";
+        }
+
+        public void muoviMacchina()
+        {
+            while (posMacchina < 685)
+            {
+                posMacchina += 30;
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(500));
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgMacchina.Margin = new Thickness(posMacchina, 63, 0, 0);
+                }));
+            }
+        }
+
+        public void muoviAereo()
+        {
+            while (posAereo < 685)
+            {
+                posAereo += 30;
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(500));
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgAereo.Margin = new Thickness(posAereo, 118, 0, 0);
+                }));
+            }
+        }
+
+        public void muoviMessi()
+        {
+            while (posMessi < 685)
+            {
+                posMessi += 30;
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(500));
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgMessi.Margin = new Thickness(posAereo, 242, 0, 0);
+                }));
+            }
+        }
+    }
+}
